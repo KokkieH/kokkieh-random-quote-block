2ed91b03f00c97b51c8d3b6a7179fd47f3836f8c
KokkieH <KokkieH@users.noreply.github.com>
2022-02-07 09:17:24 +0200

    Add content and input fields to settings page

--- kokkieh-random-quote-block.php
+++ kokkieh-random-quote-block.php
@@ -14,31 +14,91 @@
  * Domain Path:       /public/lang
  */
 
-function kokkieh_hello_world () {
-    echo 'Hello World!';
-}
+// // testing that plugin works
+// function kokkieh_hello_world () {
+//     echo 'Hello World!';
+// }
 
-add_filter( 'the_content', 'kokkieh_hello_world');
+// add_filter( 'the_content', 'kokkieh_hello_world');
 
-add_action( 'admin_menu' , 'kokkieh_create_menu' );
+// Add Settings page
+add_action( 'admin_menu' , 'kokkieh_quote_create_menu' );
 
-function kokkieh_create_menu() {
+add_action( 'admin_init' , 'kokkieh_quote_register_settings');
 
-    add_options_page( 'Random Quote Settings',
+const SETTINGS_SECTION = 'kokkieh_quote_main';
+const PAGE_SLUG = 'kokkieh_random_quote_block';
+
+// Create Settings menu item
+function kokkieh_quote_create_menu() {
+
+    add_options_page( 'Random Quote Block Settings',
         'Random Quote Block',
         'manage_options',
-        'kokkieh_quote_settings',
+        PAGE_SLUG,
         'kokkieh_quote_settings_page',
      );
 }
 
+// Create Settings page
 function kokkieh_quote_settings_page() {
     ?>
     <div class="wrap">
         <h2>Random Quote Block</h2>
         <form action="options.php" method="POST">
+            <?php
+    
+            settings_fields( PAGE_SLUG );
+            do_settings_sections( PAGE_SLUG );
+            submit_button ( 'Save Changes', 'primary' );
+
+            ?>
         </form>
     </div>
     <?php
 }
 
+// Add settings to page
+function kokkieh_quote_register_settings() {
+    add_settings_section(
+        SETTINGS_SECTION,
+        'API Credentials',
+        'kokkieh_quote_section_text',
+        PAGE_SLUG
+    );
+    add_settings_field(
+        'kokkieh_quote_username',
+        __( 'Username', 'kokkieh-random-quote-block'),
+        'kokkieh_quote_render_username',
+        PAGE_SLUG,
+        SETTINGS_SECTION
+    );
+    add_settings_field(
+        'kokkieh_quote_api_key',
+        __( 'API Key', 'kokkieh-random-quote-block'),
+        'kokkieh_quote_render_api_key',
+        PAGE_SLUG,
+        SETTINGS_SECTION
+    );
+
+    register_setting( PAGE_SLUG, 'kokkieh_quote_username');
+    register_setting( PAGE_SLUG, 'kokkieh_quote_api_key');
+}
+
+function kokkieh_quote_section_text() {
+    echo '<p>Abbreviations.com API Credentials</p>';
+}
+
+function kokkieh_quote_render_username() {
+    $options = get_option( 'kokkieh_quote_username' );
+    $name = $options[ 'name' ];
+    echo "<input id='name' name='kokkieh_quote_username[name]'
+        type='text' value='" . esc_attr( $name ) . "'/>";
+}
+
+function kokkieh_quote_render_api_key() {
+    $options = get_option( 'kokkieh_quote_api_key' );
+    $name = $options[ 'name' ];
+    echo "<input id='name' name='kokkieh_quote_api_key[name]'
+        type='text' value='" . esc_attr( $name ) . "'/>";
+}
