df97cf85ee4d57492c4462b4e4341a75d17f6201
KokkieH <KokkieH@users.noreply.github.com>
2022-02-14 10:53:46 +0200

    Make plugin translation-ready

--- kokkieh-random-quote-block.php
+++ kokkieh-random-quote-block.php
@@ -14,17 +14,28 @@
  * Domain Path:       /public/lang
  */
 
-// // testing that plugin works
-// function kokkieh_hello_world () {
-//     echo 'Hello World!';
-// }
+// testing that plugin works
+function kokkieh_hello_world () {
+    _e( 'Hello World!', 'kokkieh-random-quote-block' );
+}
 
-// add_filter( 'the_content', 'kokkieh_hello_world');
+add_filter( 'the_content', 'kokkieh_hello_world' );
+
+// Internationalize plugin
+
+add_action( 'init', 'kokkieh_quote_load_textdomain' );
+
+function kokkieh_quote_load_textdomain() {
+    load_plugin_textdomain( 'kokkieh-random-quote-block',
+        false,
+        'kokkieh-random-quote-block/languages'
+    );
+}
 
 // Add Settings page
-add_action( 'admin_menu' , 'kokkieh_quote_create_menu' );
+add_action( 'admin_menu', 'kokkieh_quote_create_menu' );
 
-add_action( 'admin_init' , 'kokkieh_quote_register_settings');
+add_action( 'admin_init', 'kokkieh_quote_register_settings' );
 
 const SETTINGS_SECTION = 'kokkieh_quote_main';
 const PAGE_SLUG = 'kokkieh_random_quote_block';
@@ -32,11 +43,12 @@ const PAGE_SLUG = 'kokkieh_random_quote_block';
 // Create Settings menu item
 function kokkieh_quote_create_menu() {
 
-    add_options_page( 'Random Quote Block Settings',
-        'Random Quote Block',
+    add_options_page( 
+        __( 'Random Quote Block Settings', 'kokkieh-random-quote-block' ),
+        __( 'Random Quote Block', 'kokkieh-random-quote-block' ),
         'manage_options',
         PAGE_SLUG,
-        'kokkieh_quote_settings_page',
+        'kokkieh_quote_settings_page'
      );
 }
 
@@ -44,10 +56,10 @@ function kokkieh_quote_create_menu() {
 function kokkieh_quote_settings_page() {
     ?>
     <div class="wrap">
-        <h2>Random Quote Block</h2>
+        <h2><?php _e( 'Random Quote Block', 'kokkieh-random-quote-block' ) ?></h2>
         <form action="options.php" method="POST">
             <?php
-    
+
             settings_fields( PAGE_SLUG );
             do_settings_sections( PAGE_SLUG );
             submit_button ( 'Save Changes', 'primary' );
@@ -62,31 +74,31 @@ function kokkieh_quote_settings_page() {
 function kokkieh_quote_register_settings() {
     add_settings_section(
         SETTINGS_SECTION,
-        'API Credentials',
+        __( 'API Credentials', 'kokkieh-random-quote-block' ),
         'kokkieh_quote_section_text',
         PAGE_SLUG
     );
     add_settings_field(
         'kokkieh_quote_username',
-        __( 'Username', 'kokkieh-random-quote-block'),
+        __( 'Username', 'kokkieh-random-quote-block' ),
         'kokkieh_quote_render_username',
         PAGE_SLUG,
         SETTINGS_SECTION
     );
     add_settings_field(
         'kokkieh_quote_api_key',
-        __( 'API Key', 'kokkieh-random-quote-block'),
+        __( 'API Key', 'kokkieh-random-quote-block' ),
         'kokkieh_quote_render_api_key',
         PAGE_SLUG,
         SETTINGS_SECTION
     );
 
-    register_setting( PAGE_SLUG, 'kokkieh_quote_username');
-    register_setting( PAGE_SLUG, 'kokkieh_quote_api_key');
+    register_setting( PAGE_SLUG, 'kokkieh_quote_username' );
+    register_setting( PAGE_SLUG, 'kokkieh_quote_api_key' );
 }
 
 function kokkieh_quote_section_text() {
-    echo '<p>Abbreviations.com API Credentials</p>';
+    echo '<p>' . __('Abbreviations.com API Credentials', 'kokkieh-random-quote-block' ) . '</p>';
 }
 
 function kokkieh_quote_render_username() {
